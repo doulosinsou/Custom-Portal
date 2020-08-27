5107612a21027285b3a01a6a1831512394274dc5
Luke-Moyer <luke@moyeraudio.com>
2020-08-27 13:53:50 -0500

    Initial Commit

---
+++ src/server/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+
+const bodyParser = require('body-parser');
+const cors = require('cors');
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(cors());
+app.use(express.static('./src/client'));
+
+const port = "3011";
+const server = app.listen(port, ()=>{
+  console.log("server started on "+port);
+});
