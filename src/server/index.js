b606869ed37b41f42af45aaffeb01aa936b35933
Luke-Moyer <luke@moyeraudio.com>
2020-09-14 14:24:52 -0500

    siplified server/index.js

--- src/server/index.js
+++ src/server/index.js
@@ -2,17 +2,15 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-const authController = require('./Auth/Auth_controller');
-const verifyToken = require('./Auth/Verify_Token');
-const user = require('./User/user.js');
-
-
 const bodyParser = require('body-parser');
 const cors = require('cors');
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
 app.use(cors());
 
+const authController = require('./Auth/Auth_controller');
+const verifyToken = require('./Auth/Verify_Token');
+const user = require('./User/user.js');
 
 const port = "3041";
 const server = app.listen(port, ()=>{
@@ -22,8 +20,7 @@ const server = app.listen(port, ()=>{
 app.use(express.static("./src/client"));
 app.use(express.static("./src/portal2"));
 
-
-app.use('/client', express.static("./src/client"));
+app.use('/', express.static("./src/client"));
 app.use('/portal', verifyToken, isValid, user, express.static('./src/portal2'));
 
 const maViews = require('./views/process');
@@ -34,7 +31,7 @@ app.set('view engine', 'ma');
 async function isValid(req, res, next){
   if (req.err){
     console.log(req.err);
-    return res.redirect(301, "/client");
+    res.redirect(301, "/");
   }else{
     next();
   }
@@ -42,7 +39,7 @@ async function isValid(req, res, next){
 
 app.post('/register', authController.register, function (req,res,next){
   console.log(req.data);
-  res.redirect(301, '/client');
+  res.redirect(301, '/');
 })
 
 app.post('/login', authController.login)
@@ -52,17 +49,6 @@ app.all('/port', verifyToken, function(req,res,next){
     console.log(req.err);
     res.send(req.err);
   }else{
-    console.log("send /portal redirect path");
     res.send({redirect: "/portal"});
   }
 })
-
-// app.use("/data", )
-
-// app.get('/port/me', async function(req,res,next){
-//   const call = "SELECT * FROM authentication WHERE ID='"+req.userId+"'";
-//   const data = await query(call);
-//   data[0].pass = "";
-//   data[0].ID = "";
-//   res.send(data[0])
-// })
